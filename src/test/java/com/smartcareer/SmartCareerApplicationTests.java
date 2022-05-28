39a49883310c107e366731b830cb067fb9b3a9bb
Abhishek Shingade <shingadeabhi111@gmail.com>
2022-05-28 20:51:46 +0530

    added test files

---
+++ src/test/java/com/smartcareer/SmartCareerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcareer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCareerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
