0712bdbf027816da20a6ce40fb27fc2d40ecdcce
Abhishek Shingade <shingadeabhi111@gmail.com>
2022-05-28 20:33:39 +0530

    Main Spring boot application file that need to be run

---
+++ src/main/java/com/smartcareer/SmartCareerApplication.java
@@ -0,0 +1,13 @@
+package com.smartcareer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCareerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCareerApplication.class, args);
+	}
+
+}
