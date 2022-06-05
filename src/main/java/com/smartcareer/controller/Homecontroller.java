0d0b85d0ca63d2a40717b0abb55d28c5459a8282
Abhishek Shingade <shingadeabhi111@gmail.com>
2022-06-05 11:33:52 +0530

    modified access denied page

--- src/main/java/com/smartcareer/controller/Homecontroller.java
+++ src/main/java/com/smartcareer/controller/Homecontroller.java
@@ -23,47 +23,49 @@ public class Homecontroller {
 
 	@Autowired
 	private ContactJpaRepository contactjpa;
-	
+
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
-	
+
 	@Autowired
 	private UserJpaRepository userRepo;
 
 	@RequestMapping("/access-denied")
-	public String denied() {
+	public String denied(Model model) {
+		model.addAttribute("title", "Access Denied !!");
 		return "access-denied";
 	}
-	
-	@RequestMapping({"/signup"})
+
+	@RequestMapping({ "/signup" })
 	public String signup(Model model) {
 		model.addAttribute("title", "SignUp page");
 		model.addAttribute("user", new User());
 		return "signup";
 	}
-	
-	@RequestMapping({"/signin","/"})
+
+	@RequestMapping({ "/signin", "/" })
 	public String singin(Model model) {
 		model.addAttribute("title", "Login page");
 		return "signin";
 	}
-	
+
 	@RequestMapping("/contact")
 	public String contact(Model model) {
 		model.addAttribute("title", "Contact us");
 		model.addAttribute("contactmail", new Contactmail());
 		return "contact";
 	}
-	
-	@RequestMapping(value = "/send",method = RequestMethod.POST)
-	public String sendcontact(@Valid @ModelAttribute("contactmail") Contactmail contactmail,BindingResult thResult,Model model,HttpSession session) {
-		
+
+	@RequestMapping(value = "/send", method = RequestMethod.POST)
+	public String sendcontact(@Valid @ModelAttribute("contactmail") Contactmail contactmail, BindingResult thResult,
+			Model model, HttpSession session) {
+
 		try {
-			if(thResult.hasErrors()) {
+			if (thResult.hasErrors()) {
 				model.addAttribute("contactmail", contactmail);
 				return "contact";
 			}
-			
+
 			Contactmail contact = this.contactjpa.save(contactmail);
 			System.out.println(contact);
 			model.addAttribute("title", "Contact us");
@@ -78,42 +80,44 @@ public class Homecontroller {
 			return "contact";
 		}
 	}
-	
-	
-	@RequestMapping(value = "/do-register",method = RequestMethod.POST)
-	public String registration(@Valid @ModelAttribute("user") User user,BindingResult theResult,Model model,HttpSession session) { 
+
+	@RequestMapping(value = "/do-register", method = RequestMethod.POST)
+	public String registration(@Valid @ModelAttribute("user") User user, BindingResult theResult, Model model,
+			HttpSession session) {
 		try {
-			
-			if(theResult.hasErrors()) {
-				System.out.println("Error "+theResult.toString());
+
+			if (theResult.hasErrors()) {
+				System.out.println("Error " + theResult.toString());
 				model.addAttribute("user", user);
 				return "signup";
 			}
-			
+
 			User theUser = userRepo.getUserByUsername(user.getEmail());
-			
-			if(theUser!=null) {
+
+			if (theUser != null) {
 				session.setAttribute("message", new Message("Username already exist", "alert-danger"));
 				return "signup";
 			}
-			
-			System.out.println("User : "+user);
+
+			System.out.println("User : " + user);
 			user.setRole("ROLE_USER");
 			user.setPassword(passwordEncoder.encode(user.getPassword()));
-			
-			
+
 			User result = this.userRepo.save(user);
 			System.out.println(result);
 			model.addAttribute("title", "Sign Up");
 			model.addAttribute("user", result);
-			session.setAttribute("message",new Message("Resgistration successful", "alert-success"));
+			session.setAttribute("message", new Message("Resgistration successful", "alert-success"));
 			return "signup";
 		} catch (Exception e) {
-			
+
 			e.printStackTrace();
-			session.setAttribute("message", new Message("Something went wrong "+e.getMessage(),"alert-danger"));
+			session.setAttribute("message", new Message("Something went wrong " + e.getMessage(), "alert-danger"));
 			model.addAttribute("user", user);
 			return "signup";
 		}
+
 	}
+
+	
 }
